8dcebbd1a842aef4f64a4d56e97e8562b531e476
김병년 <qud5252@naver.com>
2025-04-21 17:08:13 +0900

    커밋

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio/",
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-    },
-  },
+  base: "/",
 });
