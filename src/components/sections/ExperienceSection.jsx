c398f33f118d1715aa754293acd85df45b8bb6fe
KimByeongNyeon <qud5252@naver.com>
2025-06-16 17:38:42 +0900

    feat: 프로젝트 설명 간소화 및 텍스트 추가

--- src/components/sections/ExperienceSection.jsx
+++ src/components/sections/ExperienceSection.jsx
@@ -8,7 +8,7 @@ const ExperienceSection = () => {
       company: "SSAFY",
       period: "2024.10 ~ 2024.11",
       description: ["프론트엔드 전체 기능 개발 담당", "Git 관리 총괄 담당", "실시간 암호화폐 시세 확인 기능 구현", "사용자 친화적 UI/UX 설계 및 개발"],
-      tech: ["Vue.js", "Django"],
+      tech: ["Vue.js", "JavaScript"],
     },
     {
       title: "2차 프로젝트 - 문화재 현장 체험학습 앱",
@@ -21,7 +21,7 @@ const ExperienceSection = () => {
         "학생 어플: 만족도 조사, 오답노트, 마이페이지, 리워드(도감 페이지), 일정 화면 개발",
         "선생님 어플: 팀 관리, 마이페이지, 보고서 관리 화면 개발",
       ],
-      tech: ["Kotlin", "Android Studio", "Spring Boot"],
+      tech: ["Kotlin", "Android Studio"],
     },
     {
       title: "3차 프로젝트 - 개인 맞춤형 금융 교육 플랫폼",
@@ -36,7 +36,22 @@ const ExperienceSection = () => {
         "이미지 최적화 기법 적용",
         "STOMP + API를 통한 실시간 방 관리 기능 개발",
       ],
-      tech: ["React.js", "Spring Boot", "TypeScript", "STOMP"],
+      tech: ["React.js", "TypeScript", "STOMP", "pixi.js"],
+    },
+    {
+      title: "4차 프로젝트 - 개인 얼굴형 분석 및 추천 헤어스타일 플랫폼폼",
+      company: "SSAFY",
+      period: "2025.04 ~ 2025.05",
+      description: [
+        "사용자 페이지 전체 기능 개발 담당",
+        "프로젝트 구조화(Smart-dumb-component) 패턴 도입 및 훅 계층화",
+        "도메인별 훅 분리 및 전역적으로 사용할 공통 훅 작성(API, STOMP 연결 등)",
+        "face-api.js 를 통한 얼굴 인식 및 얼굴 방향 로직 구현",
+        "React-query를 통한 비동기 캐싱 전략 도입",
+        "Toss Payment API를 통한 예약 및 결제 기능 도입",
+        "삼성 청년 SW 아카데미 최종 프로젝트 기업연계 프로젝트 1위 및 최우수상 수상",
+      ],
+      tech: ["React.js", "TypeScript", "React-query", "framer-motion", "redux-toolkit", "S3"],
     },
   ];
 
--- src/components/sections/ProjectsSection.jsx
+++ src/components/sections/ProjectsSection.jsx
@@ -25,10 +25,9 @@ const ProjectsSection = () => {
       description:
         "다양한 금융상품(예/적금, 주식, 코인)을 사용자에게 추천하는 웹 애플리케이션입니다. 사용자는 추천받은 상품에 대한 의견을 나눌 수 있는 커뮤니티 게시판을 이용할 수 있으며, 각 상품에 대한 코멘트 작성이 가능합니다. AI 기반 맞춤형 금융 상품 추천과 직관적인 UI를 통해 금융 정보에 쉽게 접근할 수 있습니다.",
       image: "./assets/cashfit.png",
-      tech: ["Vue.js", "JavaScript", "Django", "Python", "SQLite", "REST API"],
+      tech: ["Vue.js", "JavaScript"],
       features: ["금융 상품 추천(예/적금, 주식, 코인)", "커뮤니티 게시판 및 댓글 기능", "로그인/회원가입 시스템", "환율 계산기", "좋아요 기능", "AI 기반 추천/검색 서비스", "상품 그래프 시각화"],
-      techReason:
-        "프론트엔드는 Vue.js와 JavaScript를 사용하여 반응형 사용자 인터페이스를 구현했으며, 백엔드로는 Django와 Python을 활용해 빠른 개발과 안정적인 API를 구축했습니다. 데이터베이스는 SQLite를 사용하여 금융 데이터를 효율적으로 관리했으며, REST API를 통해 프론트엔드와 백엔드 간의 원활한 통신을 구현했습니다.",
+      techReason: "프론트엔드는 Vue.js와 JavaScript를 사용하여 반응형 사용자 인터페이스를 구현했으며, UpBit API, Candle Chart library를 통한 실시간 암호화폐 기능을 구현하였습니다.",
       period: "2024.10 ~ 2024.11",
       role: "팀장, 프론트엔드 총괄",
       links: {
@@ -42,10 +41,10 @@ const ProjectsSection = () => {
       description:
         "현장학습에서 학생들은 사진 인증의 번거로움, 단순 관람과 불분명한 학습 목적으로 인한 지루함을 호소하고 있으며, 인솔자들은 실시간 활동 현황 파악의 어려움과 체계적인 관리 도구의 부재로 고민하고 있습니다. 이러한 문제를 해결하고자 GPS 위치 서비스와 게임적 요소를 접목한 새로운 교육용 앱을 기획했습니다. 학생들은 퀴즈와 미션을 해결하며 문화재를 즐겁게 배울 수 있고, 인솔자들은 실시간 모니터링과 체계적인 관리 도구를 활용할 수 있어 보다 효율적인 현장학습이 가능해집니다.",
       image: "./assets/MBG.png",
-      tech: ["Kotlin", "Android Studio", "Retrofit2", "OKHTTP3", "Glide", "Hilt", "Spring Boot", "MySQL", "Firebase", "JWT", "Spring Security"],
+      tech: ["Kotlin", "Android Studio"],
       features: ["GPS 위치 기반 문화재 정보 제공", "퀴즈와 미션 기반 게이미피케이션", "인솔자용 실시간 학생 활동 모니터링", "체계적인 현장학습 관리 도구"],
       techReason:
-        "클라이언트는 Kotlin으로 개발하여 안드로이드 앱의 안정성과 간결한 코드 작성이 가능했습니다. Retrofit2와 OKHTTP3로 효율적인 네트워크 통신을 구현했으며, Glide를 통해 이미지 로딩을 최적화했습니다. 의존성 주입에는 Hilt를 활용하여 코드의 유지보수성을 향상시켰습니다. 서버 측에서는 Spring Boot와 Java를 기반으로 안정적인 API를 구축했으며, MySQL로 관계형 데이터를 관리하고 Firebase를 통해 실시간 데이터 동기화와 푸시 알림 기능을 구현했습니다. JWT와 Spring Security를 적용하여 안전한 사용자 인증 시스템을 제공합니다.",
+        "클라이언트는 Kotlin으로 개발하여 안드로이드 앱의 안정성과 간결한 코드 작성이 가능했습니다. Retrofit2와 OKHTTP3로 효율적인 네트워크 통신을 구현했으며, Glide를 통해 이미지 로딩을 최적화했습니다. 의존성 주입에는 Hilt를 활용하여 코드의 유지보수성을 향상시켰습니다.",
       period: "2025.01 ~ 2025.02",
       role: "안드로이드 개발 총괄, 깃 총괄",
       links: {
@@ -59,10 +58,9 @@ const ProjectsSection = () => {
       description:
         "FinCatch는 사용자의 소비 데이터를 분석하여 금융 습관을 시각적으로 제공합니다. 맞춤형 금융 교육 콘텐츠를 제공하여 사용자의 관심 분야를 공부할 수 있습니다. 개인의 소비 패턴을 분석, 학습 방향을 제안합니다. 금융 배틀을 통해 포인트를 얻고 다양한 고양이를 획득할 수 있는 게이미피케이션 요소를 적용했습니다.",
       image: "./assets/fincatch.png",
-      tech: ["React.js", "TypeScript", "SpringBoot", "SpringSecurity", "Hibernate", "FastAPI", "PostgreSQL", "Redis"],
+      tech: ["React.js", "TypeScript"],
       features: ["소비 데이터 분석 및 금융 습관 시각화", "맞춤형 금융 교육 콘텐츠 제공", "개인 소비 패턴 분석 및 학습 방향 제안", "금융 배틀 및 고양이 캐릭터 수집 게이미피케이션"],
-      techReason:
-        "프론트엔드는 사용자 인터페이스의 복잡성과 상태 관리를 위해 React.js와 TypeScript를 사용했습니다. 백엔드는 안정적인 API와 확장성을 고려하여 SpringBoot와 SpringSecurity로 구현하였고, 일부 데이터 처리 기능은 FastAPI를 통해 빠른 응답 속도를 보장했습니다. 데이터베이스는 안정적인 관계형 데이터 저장을 위해 PostgreSQL을 사용하고, 캐싱과 실시간 처리를 위해 Redis를 도입했습니다. Hibernate는 객체 관계 매핑을 통해 데이터베이스 작업을 단순화하는 데 큰 역할을 했습니다.",
+      techReason: "프론트엔드는 사용자 인터페이스의 복잡성과 상태 관리를 위해 React.js와 TypeScript를 사용했습니다. 또한, pixi.js 를 통해 Sprite 이미지를 움직이는 애니메이션으로 구현하였습니다.",
       period: "2025.02 ~ 2025.04",
       role: "프론트엔드 개발, 게이미피케이션 요소 설계, 데이터 시각화",
       links: {
@@ -74,13 +72,12 @@ const ProjectsSection = () => {
       title: "toBe Continued",
       subtitle: "얼굴형 맞춤 헤어스타일 추천 웹 애플리케이션",
       description:
-        "비컨 기업과 연계된 프로젝트로, 사용자의 얼굴형을 분석하여 최적의 헤어스타일을 추천하는 웹 애플리케이션입니다. 머신러닝 모델을 활용해 정확한 얼굴형 분석과 개인화된 추천 서비스를 제공합니다.",
-      image: "./assets/backgroud.jpg",
-      tech: ["React.js", "Spring Boot", "OpenCV", "AWS", "PostgreSQL"],
-      features: ["얼굴형 실시간 분석", "개인화된 헤어스타일 추천", "가상 헤어스타일 시뮬레이션", "전문 스타일리스트 연결 서비스"],
-      techReason:
-        "클라이언트 측 얼굴 인식과 분석을 위해 React.js와 OpenCV 를 선택했습니다. 이를 통해 사용자 브라우저에서 직접 얼굴 분석이 가능해 개인정보 보호와 실시간 상호작용을 개선했습니다. 백엔드는 Spring Boot를 통해 확장 가능한 마이크로서비스 아키텍처를 구현했으며, PostgreSQL은은 다양한 헤어스타일 데이터와 사용자 프로필을 유연하게 저장하기 위해 선택했습니다. AWS 클라우드 서비스를 활용해 이미지 처리와 머신러닝 모델 배포를 효율적으로 관리하고 있습니다.",
-      period: "2025.04 ~ 현재 진행중",
+        "비컨 기업과 연계된 프로젝트로, 사용자의 얼굴형을 분석하여 최적의 헤어스타일을 추천하는 웹 애플리케이션입니다. 머신러닝 모델을 활용해 정확한 얼굴형 분석과 개인화된 추천 서비스를 제공합니다 해당 프로젝트는 (주)Becon과 연계한 프로젝트로, 삼성 청년 소프트웨어 아카데미 12기 최종 프로젝트 상위 6팀 및 최우수상 수상 프로젝트 입니다.",
+      image: "./assets/logo.png",
+      tech: ["React.js", "Spring Boot", "face-api.js"],
+      features: ["얼굴형 실시간 분석", "개인화된 헤어스타일 추천", "가상 헤어스타일 시뮬레이션", "미용실 예약 시스템"],
+      techReason: "클라이언트 측 얼굴 인식과 분석을 위해 React.js와 face-api.js 를 선택했습니다. 이를 통해 사용자 브라우저에서 직접 얼굴 분석이 가능합니다.",
+      period: "2025.04 ~ 2025.05",
       role: "팀장, 프론트 엔드 총괄",
       links: {
         github: "https://github.com/",
